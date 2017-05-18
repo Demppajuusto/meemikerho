5fafbce9a6a0fcfa3f22c05696e82e4ab72358e9
Vieras Oppilas <vieras@munkka.local>
2017-05-18 15:49:26 +0300

    Add all files to git

---
+++ serveri.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html", rnd=random.randint(1,10))
+
+@app.route('/koodauskerho')
+def hello_world():
+    return "????"
+
+if __name__ == "__main__":
+    app.run()
